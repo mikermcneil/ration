16f8f2bad8125fc63d6dd89846bc0789f499a902
Rachael Shaw <r@rachael.wtf>
2017-12-08 01:45:18 -0600

    Add a minimal 'Thing' model

---
+++ api/models/Thing.js
@@ -0,0 +1,48 @@
+/**
+ * Thing.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    imageUploadFd: {
+      type: 'string',
+      description: 'The Skipper file descriptor string uniquely identifying the uploaded image.',
+      required: true
+    },
+
+    imageUploadMime: {
+      type: 'string',
+      description: 'The MIME type for the uploaded image.',
+      required: true
+    },
+
+    label: {
+      type: 'string',
+      description: 'A (very) brief description of the item.',
+      example: 'Waffle Iron'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    owner: {
+      model: 'User'
+    }
+
+  },
+
+};
+
