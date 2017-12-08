488108a844f7160acbf4b98d12a00b6df60e70b8
Rachael Shaw <r@rachael.wtf>
2017-12-08 01:38:29 -0600

    Add stubbed-out form content to things page

--- assets/js/pages/things/things.page.js
+++ assets/js/pages/things/things.page.js
@@ -3,8 +3,35 @@ parasails.registerPage('things', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    //…
+
+    friends: [],
+
+    // The "virtual" portion of the URL which is managed by this page script.
+    virtualPageSlug: '',
+
+    // Form data
+    formData: {
+      // ...
+    },
+
+    // For tracking client-side validation errors in our form.
+    // > Has property set to `true` for each invalid property in `formData`.
+    formErrors: { /* … */ },
+
+    // Syncing / loading state
+    syncing: false,
+
+    // Server error state
+    cloudError: '',
+
+    // Success state when form has been submitted
+    cloudSuccess: false,
+
   },
+
+  virtualPages: true,
+  html5HistoryMode: 'history',
+  virtualPagesRegExp: new RegExp(/^\/things\/?([^\/]+)?/),
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
@@ -18,6 +45,38 @@ parasails.registerPage('things', {
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    //…
+    clickAddButton: function() {
+      // Open the modal.
+      this.goto('/things/new');
+    },
+
+    openedUploadThingModal: function() {
+      // TODO: focus first field
+    },
+
+    closeUploadThingModal: function() {
+      this.goto('/things');
+    },
+
+    handleParsingUploadThingForm: function() {
+      // Clear out any pre-existing error messages.
+      this.formErrors = {};
+
+      var argins = this.formData;
+
+      // If there were any issues, they've already now been communicated to the user,
+      // so simply return undefined.  (This signifies that the submission should be
+      // cancelled.)
+      if (Object.keys(this.formErrors).length > 0) {
+        return;
+      }
+
+      return argins;
+    },
+
+    submittedUploadThingForm: function() {
+      // Add the new thing to the list
+      // TODO
+    },
   }
 });
